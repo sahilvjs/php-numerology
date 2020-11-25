f98c44abb6bd4f190012019e2940f9c1a580c78a
Sahilvjs <sahilvjs@gmail.com>
2020-11-25 22:30:13 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<body>
+
+	<h1>Numerology</h1>
+	<p>Please enter the user details</p>
+
+
+	<form action="show_details.php">
+
+		<table>
+			<tr>
+		    	<td>
+		    		<label for="name">Name:</label>		<br><br>
+				</td>
+				<td style="padding-left: 10px">
+		    		<input type="text" id="name" name="name">	<br><br>
+				</td>
+			</tr>
+			<tr>
+		    	<td>
+		    		<label for="dob">Date of Birth:</label><br><br>
+				</td>
+				<td style="padding-left: 10px">
+		    		<input type="date" id="dob" name="dob"><br><br>
+				</td>
+			</tr>
+			<tr>
+		    	<td>
+		    		<label for="gender">Gender:</label><br><br>
+				</td>
+				<td style="padding-left: 10px">
+		    		<input type="radio" id="male" name="gender" value="male" checked="checked"">
+					<label for="male">Male</label><br>
+					<input type="radio" id="female" name="gender" value="female">
+					<label for="female">Female</label>	
+				</td>
+			</tr>
+			<tr>
+		    	<td>
+		    		<input type="submit" value="Submit">	
+				</td>
+			</tr>
+		</table>	
+
+	</form>
+
+	</body>
+</html>
